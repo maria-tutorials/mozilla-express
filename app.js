5b130c8d7d7e04657097e01e92a434096f72322a
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2018-06-21 16:51:15 +0100

    Added express to run the app

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res) {
   res.send('Hello World !!!');
 });
 
+app.all('/secret', function(req, res, next) {
+  console.log('Accessing the secret section ...');
+  next(); // pass control to the next handler
+});
+
 //starts server
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');

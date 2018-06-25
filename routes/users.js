37004ee89cb4c8455651cd9dc57764b156722b8f
Maria Inês Serra <ines.serra.95@gmail.com>
2018-06-25 17:43:20 +0100

    Adds /users/cool route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
 	res.send('respond with a resource');
 });
 
+router.get('/cool', function (req, res, next) {
+	res.send('You\'re so cool 8D');
+});
+
 module.exports = router;

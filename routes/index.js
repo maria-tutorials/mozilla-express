bd11af3145b46d4d83277f5005b2eba22e223c08
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2019-08-08 15:27:24 +0100

    updates

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page - using new catalog routes that uses controllers */
-router.get('/', function(req, res) {
-    res.redirect('/catalog');
-  });
+router.get('/', function (req, res) {
+	res.redirect('/catalog');
+});
 
 module.exports = router;
